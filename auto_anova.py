e0676b561e274ac13340fd5c80be4f86850b79e2
Alex Staritsky <alexstaritsky@hotmail.nl>
2017-01-02 13:41:32 +0100

    Functies
    Functies bedacht die in het programma mogelijk moeten

--- auto_anova.py
+++ auto_anova.py
@@ -4,7 +4,26 @@ Martijn Landman en Alex Staritsky
 Datum: 28-12-2016
 """
 
+from scipy import stats
+import pandas as pd
+
+def read_file_locus(path):
+    # Lees het locusbestand
+
+def read_file_treat(path):
+    # Lees het treatbestand
+
+def create_groups(locus_data, treat_data):
+    # Maak de a en de b groep
+    
+def single_factor_anova(group_a, group_b):
+    # Doe een single factor ANOVA 
+
+def save_file(path, data, sep="\t"):
+    # Sla het bestand op
+
 # Main functie
 def main():
-  print("Hello World!")
+    print("Hello World!")
+    
 main()
