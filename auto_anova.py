dbd30635d1e525d115203e1ad2838ec5266b9427
Alex Staritsky <alexstaritsky@hotmail.nl>
2017-01-06 13:37:46 +0100

    +Create groups; +Basis IO; +Commentaar
    Create groups functie toegevoegd. Basis IO voor het lezen en schrijven
    toegevoegd. Nog wat commentaar toegevoegd. Als het goed is moeten nu nog
    de lees en schijf functies afgemaakt worden en moet het programma getest
    worden.

--- auto_anova.py
+++ auto_anova.py
@@ -7,11 +7,15 @@ Datum: 28-12-2016
 from scipy import stats
 
 # Gene class om data op te slaan
+# p_value is een float
+# f_value is een float
+# locus is een String
+# markers is een lijst met strings die "a" of "b" zijn
 class Gene:
     
     def __init__(self, locus, markers):
-        self.__p_value = 0
-        self.__f_value = 0
+        self.__p_value = float(0)
+        self.__f_value = float(0)
         self.__locus = locus
         self.__markers = markers
         
@@ -35,26 +39,47 @@ class Gene:
 
 # Lees het locusbestand met markerdata
 def read_file_locus(path):
-    raise NotImplementedError
+    genes = []
+    with open(path, "r") as bestand:
+        for regel in bestand:
+            # Doe iets
+            raise NotImplementedError
+    bestand.close()
+    return genes
 
-# Lees het traitbestand met traitnames
+# Lees het traitbestand met traitnames -> een lijst met strings die een kommagetal of "*" zijn
 def read_file_trait(path):
-    raise NotImplementedError
+    traits = []
+    with open(path, "r") as bestand:
+        for regel in bestand:
+            # Doe iets
+            raise NotImplementedError
+    bestand.close()
+    return traits
 
 # Maak de a en de b groep
 def create_groups(traits, markers):
-    raise NotImplementedError
+    a, b = [], []
+    for i in range(len(markers)):
+        if markers[i] == "a" and traits[i] != "*":
+            a.append(float(traits[i]))
+        if markers[i] == "b" and traits[i] != "*":
+            b.append(float(traits[i]))
+    return a, b
 
 # Sla het eindbestand bestand op
 def save_file(path, data, sep="\t"):
-    raise NotImplementedError
+    with open(path, "w") as bestand:
+        for regel in bestand:
+            # Doe iets
+            raise NotImplementedError
+    bestand.close()
 
 # Main functie
 def main():
     # Lees de bestanden; traits is een lijst met alle traitnames; genes is een lijst met gene objecten die een locus en markerdata bevat
-    traits = read_file_trait("CvixLer.qua") 
-    genes = read_file_locus("CvixLer.loc")
-    
+    traits = read_file_trait("CvixLer.qua")
+    genes = read_file_locus("CvixLer.loc") 
     # Voor elk gen (of locus)
     for gene in genes:
         # Maak de 2 groepen voor de ANOVA
@@ -65,6 +90,6 @@ def main():
         gene.set_f_value(f)
         gene.set_p_value(p)
     # Sla het bestand op (waarschijnlijk tab-delimited op deze manier voor elk gen "locus \t f-value \t p-value \n") en dan geen spaties tussen
-    save_file(genes)
+    save_file("results_auto_anova.txt", genes)
     
 main()
